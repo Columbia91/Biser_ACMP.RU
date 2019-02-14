d1610c057f5d96ecdb557505de9023a2874f5beb
cougar91 <alpha.z91@mail.ru>
2019-02-14 17:36:22 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\ADILET\source\repos\ConsoleApp21\ConsoleApp21\input.txt");
+            StreamWriter sw = new StreamWriter(@"C:\Users\ADILET\source\repos\ConsoleApp21\ConsoleApp21\output.txt");
+            int count = int.Parse(sr.ReadToEnd()) + 1;
+            sw.WriteLine(count);
+            sw.Close();
+        }
+    }
+}
